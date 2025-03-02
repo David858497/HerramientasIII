925f0c4a72f267a9f8e92b919c42eece72492a40
juanj <juanj@LAPTOP-P4NKA30U>
2025-03-01 20:00:34 -0500

    Capa Dominio y Persistencia

---
+++ Feriados.Infraestructura.Persistencia/Contexto/FeriadosContext.cs
@@ -0,0 +1,32 @@
+﻿using Feriados.Dominio.Entidades;
+
+namespace Feriados.Infraestructura.Persistencia.Contexto
+{
+    public class FeriadosContext: DbContext
+    {
+        public FeriadosContext(DbContextOptions<FeriadosContext> options) : base(options)
+        {
+        }
+        public DbSet<Festivo> Festivos { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Festivo>(entidad =>
+            {
+                entidad.HasKey(e => e.Id);
+                entidad.HasIndex(e => e.Nombre).IsUnique();
+            });
+
+            modelBuilder.Entity<Festivo>()
+               .HasOne(e => e.Tipo)
+               .WithMany()
+               .HasForeignKey(e => e.Id);
+
+            modelBuilder.Entity<Tipo>(entidad =>
+            {
+                entidad.HasKey(e => e.Id);
+                entidad.HasIndex(e => e.Nombre).IsUnique();
+            });
+        }
+    }
+}
